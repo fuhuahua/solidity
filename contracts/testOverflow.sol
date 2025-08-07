5c62ed144c9775f8519b0664785136727c4c845e
fuhuahua <huxingkong2011@sina.com>
2025-08-07 16:46:30 +0800

    testoverflow

---
+++ contracts/testOverflow.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract testOverflow {
+    function add1() public pure returns (uint8) {
+        uint8 x = 200;
+        uint8 y = x*2;
+        return y;
+    }
+
+    function add2() public pure returns (uint8){
+        uint8 i = 240;
+        uint8 j = 1;
+        uint8 k = i + j;
+        return k;
+    }
+}
